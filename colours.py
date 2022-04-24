7a7d97bd89ab6c16e71f6227a5c84deafa39dc43
DanPearce <daniel_pearce@outlook.com>
2022-04-24 14:34:45 +0000

    Import colorama

---
+++ colours.py
@@ -0,0 +1,11 @@
+"""
+The Colours.py script is used to enhance
+the user experience of the game by
+adding colours using Colorama.
+The colours are intended to make the game
+more visually appealing and to create
+an unique experience for the user.
+"""
+# Language and Script imports
+import colorama
+from colorama import init, Fore, Back, Style
