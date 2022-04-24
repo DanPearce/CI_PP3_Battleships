283f8051cc59a020ef9e5813a986b21f3c647aac
DanPearce <daniel_pearce@outlook.com>
2022-04-24 14:25:11 +0000

    Add initial functions to run.py with supporting comments

---
+++ run.py
@@ -0,0 +1,55 @@
+"""
+Welcome to Battleships!
+The command-line version of Battleships
+Developed by Dan Pearce using the Python language!
+"""
+# Langauge and Script imports
+
+# Global Variables
+
+# Main Functions
+
+
+def title():
+    """
+    Display the game's main 'title' screen.
+    Game name and development credit.
+    """
+
+
+def clear():
+    """
+    Clears the console to be more visually appealing to the user
+    """
+
+
+def new_line():
+    """
+    Prints a line, visually matching the game's title colours
+    Creates more readability in the console
+    """
+
+
+def game_menu():
+    """
+    Displays the 'menu' to the user
+    Allowing the user to either
+    view the rules
+    or to play the game
+    """
+
+
+def rules():
+    """
+    Displays to the user the rules of the game
+    and how to play the game.
+    """
+
+
+def start_game():
+    """
+    When called, this function will start the game
+    and initiate the relevent functions as the
+    game progresses.
+    """
+
